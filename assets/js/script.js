9260cd2a60f83f7ad7d2835062b0f710e0d4cd19
Terezia Dujavova <tmdujavova@gmail.com>
2017-01-17 19:00:25 +0100

    verzia 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ description.hide();
 
 		description.not( newDesc ).hide();
 		
-		newDesc.fadeIn(500);
+		newDesc.show().addClass('fadeInRight');
 	}
 
 	showDesc( active );
@@ -29,6 +29,28 @@ description.hide();
 
 		showDesc( $(this) );
 		event.preventDefault();
+	});
+
+
+
+
+	var backToTop = $('<a>', { 
+			href: '#domov', 
+			class: 'back-to-top',
+			html: '<i class="fa fa-caret-up fa-5x"></i>'
+		});
+
+	backToTop
+		.hide()
+		.appendTo('body')
+		.on('click', function() {
+			$('html,body').animate({ scrollTop: 0 }, 2000);
+		});
+
+	var win = $(window);
+	win.scroll(function() {
+		if ( win.scrollTop() >= 300 ) backToTop.fadeIn();
+		else backToTop.hide();
 	});
 
 	
